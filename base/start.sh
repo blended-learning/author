6302623126ad8fc18f9b414e9a27f47fbda3c8c9
Ken Pu <ken.pu@uoit.net>
2023-05-02 10:44:39 -0400

    creation

---
+++ base/start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#
+# Trust all sudoers
+#
+echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+
+#
+# Create the users in /etc/users.*
+#
+/bin/createusers
+
+
+
+service ssh start
+jupyterhub --Spawner.default_url=/lab -f /etc/jupyterhub_config.py
+
