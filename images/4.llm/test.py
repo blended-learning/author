5ea823ef3de71b657a0602511e4ac9363fa9dcc8
Ken Pu <ken.pu@ontariotechu.net>
2023-11-01 15:59:50 -0400

    update dockerfiles

--- images/4.llm/test.py
+++ images/4.llm/test.py
@@ -1,5 +1,9 @@
 import torch
-import lightning
+import transformers
+import accelerate
+import datasets
+import evaluate
+import peft
 
 print("Cuda devices")
 for i in range(torch.cuda.device_count()):
@@ -8,5 +12,7 @@ for i in range(torch.cuda.device_count()):
 print("Versions")
 print("cuda:", torch.version.cuda)
 print("torch:", torch.__version__)
-print("lightning:", lightning.__version__)
+print("transformers:", transformers.__version__)
+print("datasets:", datasets.__version__)
+print("peft:", peft.__version__)
 
